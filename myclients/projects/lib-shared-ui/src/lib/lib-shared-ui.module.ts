aab256bc933e727f90ec46ae258531dfde8a9560
Francois Smit <francoissmt@gmail.com>
2022-12-14 10:11:16 +0200

    Setup of projects

---
+++ myclients/projects/lib-shared-ui/src/lib/lib-shared-ui.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { LibSharedUiComponent } from './lib-shared-ui.component';
+
+
+
+@NgModule({
+  declarations: [
+    LibSharedUiComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    LibSharedUiComponent
+  ]
+})
+export class LibSharedUiModule { }
